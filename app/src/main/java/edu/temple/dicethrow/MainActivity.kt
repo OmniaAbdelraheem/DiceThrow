6fade24a64511c40659d0b03f8d4fc58a3cdf4c6
OmniaAbdelraheem <tur52752@temple.edu>
2024-10-17 11:37:50 -0400

    added +1 to get ride of the 0 and added onClickListener

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,14 +1,23 @@
 package edu.temple.dicethrow
 
 import android.os.Bundle
+import android.view.View.OnClickListener
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView.OnCloseListener
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.rollDiceButton).setOnClickListener(){
+
+        }
+        (supportFragmentManager.findFragmentById(R.id.dieContainer) as DieFragment).throwDie()
     }
 }
