ce4e16b3b719603ac489a98c97f8a7df7976fcb6
OmniaAbdelraheem <tur52752@temple.edu>
2024-10-23 21:36:27 -0400

    added +1 to get ride of the 0 and added onClickListener I changed the roll dice number to 100 and added companion object! Added FragmentContainerView for both ButtonFragment and DieFragment in activity_main.xml and ensured each fragment has its own designated container and applied layout constraints Updated ButtonFragment to use an interface for handling button clicks and notifying MainActivity and used the ButtonInterface to communicate button clicks from ButtonFragment to MainActivity, added ButtonClicked() method in MainActivity to trigger the dice roll in DieFragment when the button is clicked

---
+++ app/src/main/java/edu/temple/dicethrow/ButtonFragment.kt
@@ -0,0 +1,27 @@
+package edu.temple.dicethrow
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class ButtonFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_button, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.button).setOnClickListener {
+            (requireActivity() as ButtonInterface).ButtonClicked()
+        }
+    }
+    interface ButtonInterface {
+        fun ButtonClicked()
+    }
+}
